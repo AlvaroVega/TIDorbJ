adf7faf2919eb99403ec267585dc6a351ad28447
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2018-09-13 12:06:42 +0200

    remove folder

--- dist/bin/printIOR.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set PRINT_IOR_CLASSPATH=.;%TIDORBJ_HOME%\lib\tidorbj.jar
-set JVM=%JAVA_HOME%\bin\java
-
-%JVM% -classpath %PRINT_IOR_CLASSPATH% es.tid.TIDorbj.tools.PrintIOR %1
-
-set PRINT_IOR_CLASSPATH=
-set JVM=
--- dist/bin/tidorbj.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set +u
-
-JVM=$JDK_HOME/bin/java
-
-$JVM -classpath .:$TIDORBJ_HOME/lib/tidorbj.jar $*
